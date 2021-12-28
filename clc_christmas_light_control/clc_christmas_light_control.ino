123d00d024ab810f5dfdcf6bff74054f96d68841
Nicola Franceschinis <nicola.franceschinis@outlook.com>
2021-12-28 20:37:03 +0100

    Developed stable version for esp8266

--- clc_christmas_light_control/clc_christmas_light_control.ino
+++ clc_christmas_light_control/clc_christmas_light_control.ino
@@ -4,12 +4,53 @@
   Mail: cjosulab@gmail.com
   Start develop: 20211220
 */
-#define LOADPIN 11
-void setup() {
-  Serial.begin(9600);
-  pinMode(LOADPIN, OUTPUT);
-}
 
-void loop() {
-  serialHandler();
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+#define LOAD_PIN LED_BUILTIN
+#define LED_ON LOW
+#define LED_OFF HIGH
+ 
+const char* ssid = "WIFI-Tele1_d37b";
+const char* password = "153624abDF";
+ 
+ESP8266WebServer server(80);
+
+bool ledStatus = false;
+ 
+void setup(void) {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+ 
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  pinMode(LOAD_PIN, OUTPUT);
+ 
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+ 
+  server.on("/", handleRootDefault);
+ 
+  server.on("/ledon", handleLedOn);
+  server.on("/ledoff", handleLedOff);
+  server.on("/fading", handleFading);
+  server.on("/flashing", handleFlashing);
+ 
+  server.onNotFound(handleNotFound);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+ 
+void loop(void) {
+  server.handleClient();
 }
--- clc_christmas_light_control/functions.ino
+++ clc_christmas_light_control/functions.ino
@@ -1,36 +1,139 @@
-void serialHandler() {
-  if (Serial.available()) {
-    char buffer = Serial.read();
-    /*switch(buffer) {
-      case '0':
-      Serial.print(buffer);
-      Serial.println(" --> tree OFF");
-      lightControl(0);
-      Serial.flush();
-      break;
-      case '1':
-      Serial.print(buffer);
-      Serial.println(" --> tree ON");
-      lightControl(1);
-      Serial.flush();
-      break;
-      
-      default:
-      Serial.println("buffer");
-      break;
-    }*/
-    if(buffer >= 48 && buffer <= 57)
-      lightControl(buffer);
+#define FADE_AMOUNT 5
+#define FADE_SPEED 200
+#define FLASH_SPEED 880
+
+String handleRoot(bool ledStatus = false) {
+    String ptr =
+            "<html>\
+              <head>\
+                <title>CLC Christmas light control</title>\
+                <style>\
+                  body { background-color: #00ffff4d; font-family: Arial, Helvetica, Sans-Serif; Color: blue; }\
+                </style>\
+              </head>\
+              <body>\
+                <center>\
+                <h1>Albero smart v 1.0</h1>\
+                ";
+ 
+    if (ledStatus) {
+        ptr +=
+                "<p>Albero: ON</p><a class=\"button button-off\" href=\"/ledoff\">OFF</a>\n";
+    } else {
+        ptr +=
+                "<p>Albero: OFF</p><a class=\"button button-on\" href=\"/ledon\">ON</a>\n";
+    }
+    ptr += "<p><a class=\"button button-fading\" href=\"/fading\">FADING</a>\n</p>";
+    ptr += "<p><a class=\"button button-flashing\" href=\"/flashing\">FLASHING</a>\n</p>";
+    ptr += "</center>\
+            </body>\
+            </html>";
+ 
+    return ptr;
+}
+ 
+void handleRootDefault(){
+    ledStatus = false;
+    digitalWrite(LOAD_PIN, LED_OFF);
+    Serial.println("Albero Status: OFF");
+ 
+    server.send(200, "text/html", handleRoot(ledStatus));
+}
+ 
+void handleLedOn() {
+  ledStatus = true;
+  Serial.println("Albero Status: ON");
+  //digitalWrite(LOAD_PIN, LED_ON);
+  lightControl(1);
+  server.send(200, "text/html", handleRoot(ledStatus));
+}
+ 
+void handleLedOff() {
+  ledStatus = false;
+  Serial.println("Albero Status: OFF");
+  //digitalWrite(LOAD_PIN, LED_OFF);
+  lightControl(0);
+  server.send(200, "text/html", handleRoot(ledStatus));
+}
+
+void handleFading() {
+  ledStatus = true;
+  Serial.println("Albero Status: Fading");
+  lightControl(2);
+  server.send(200, "text/html", handleRoot(ledStatus));
+}
+
+void handleFlashing() {
+  ledStatus = true;
+  Serial.println("Albero Status: Flashing");
+  lightControl(2);
+  server.send(200, "text/html", handleRoot(ledStatus));
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+ 
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
   }
+ 
+  server.send(404, "text/plain", message);
 }
 
 void lightControl(int mode) {
   switch (mode) {
     case 0:
-    digitalWrite(LOADPIN, LOW);
+    digitalWrite(LOAD_PIN, LOW);
     break;
     case 1:
-    analogWrite(LOADPIN, 255);
+    digitalWrite(LOAD_PIN, HIGH);
     break;
+    case 2:
+    fader();
+    break;
+    case 3:
+    flasher();
+    break;
+  }
+}
+
+void fader() {
+  int pointer = 0;
+  unsigned long lastTimestamp = 0;
+  while (pointer <= 51) {
+    analogWrite(LOAD_PIN, pointer * FADE_AMOUNT);
+    if(millis() - lastTimestamp > 20) {
+      pointer++;
+      lastTimestamp = millis();
+    }
+  }
+  while (pointer >= 0) {
+    analogWrite(LOAD_PIN, pointer * FADE_AMOUNT);
+    if(millis() - lastTimestamp > FADE_SPEED) {
+      pointer--;
+      lastTimestamp = millis();
+    }
+  }
+}
+
+void flasher () {
+  int pointer = 0;
+  unsigned long lastTimestamp = 0;
+  while (pointer < 100) {
+    if(pointer % 2)
+      digitalWrite(LOAD_PIN, HIGH);
+    else
+      digitalWrite(LOAD_PIN, LOW);
+    if(millis() - lastTimestamp > FLASH_SPEED) {
+        pointer++;
+        lastTimestamp = millis();
+    }
   }
 }
